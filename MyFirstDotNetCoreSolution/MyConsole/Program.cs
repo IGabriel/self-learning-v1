e6e04f2bd5e8afff97b159506e4e56f53afbfe23
IGabriel <gabriel_cgq@hotmail.com>
2018-12-23 22:05:41 +0800

    miss  something...

--- MyFirstDotNetCoreSolution/MyConsole/Program.cs
+++ MyFirstDotNetCoreSolution/MyConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MyLibrary;
 
 namespace MyConsole
 {
@@ -6,7 +7,10 @@ namespace MyConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Human h1 = new Human("Abc");
+            Human h2 = new Human("bcd");
+
+            Console.WriteLine("H1: {0}; H2： {1}", h1, h2);
         }
     }
 }
---
+++ MyFirstDotNetCoreSolution/MyLibrary/Human.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace MyLibrary
+{
+    public class Human
+    {
+        public static readonly string ToStringFormat = "Id: {0}, Name: {1}";
+        private static int currentId = 0;
+
+        public int Id { get; }
+        public string Name { get; }
+
+        public Human() => Id = GetNextId();
+
+        public Human(string name) : this() => this.Name = name;
+
+        private static int GetNextId() => Interlocked.Increment(ref currentId);
+
+        public override string ToString() => string.Format(ToStringFormat, Id, Name);
+    }
+}
